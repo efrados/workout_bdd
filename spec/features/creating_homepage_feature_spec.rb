afbd7477ca759d9674af7c3a97d10d533812357d
Efrain Arreche <efrados@gmail.com>
2021-04-15 17:15:35 -0300

    Impement home page

--- spec/features/creating_homepage_feature_spec.rb
+++ spec/features/creating_homepage_feature_spec.rb
@@ -5,10 +5,10 @@ RSpec.feature "Listing Articles" do
   scenario "with articles created and user not singed in" do
     visit "/"
 
+    expect(page).to have_link('Home')
+    expect(page).to have_link('Athletes Den')
     expect(page).to have_content('Workout Lounge!') 
     expect(page).to have_content('Show off your workout') 
-    expect(page).not_to have_link('Home')
-    expect(page).not_to have_link('Athletes Den')
   end
 
 end
