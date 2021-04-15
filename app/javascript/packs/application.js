5122da61a8eb95e758a1ec63ee4b09109a7b0361
Efrain Arreche <efrados@gmail.com>
2021-04-15 18:25:15 -0300

    bootstrap 5 added navbar created for application

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,17 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+// import 'bootstrap/js/src/alert'  
+// import 'bootstrap/js/src/button'  
+// import 'bootstrap/js/src/carousel'  
+import 'bootstrap/js/src/collapse'  
+import 'bootstrap/js/src/dropdown'  
+// import 'bootstrap/js/src/modal'  
+// import 'bootstrap/js/src/popover'  
+import 'bootstrap/js/src/scrollspy'  
+// import 'bootstrap/js/src/tab'  
+// import 'bootstrap/js/src/toast'  
+// import 'bootstrap/js/src/tooltip'  
 
 Rails.start()
 Turbolinks.start()
