2a6527a8b834a28368519c9313412af4f22752fb
Efra <efrados@gmail.com>
2021-04-16 20:22:15 -0300

    Added rspec scenario for index with no exercises

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -35,4 +35,19 @@ RSpec.feature "Listing exercises" do
     expect(page).not_to have_content(@e3.workout_date)
 
   end
+
+  scenario "with logged in user and no exercises created" do
+    @e1.destroy
+    @e2.destroy
+
+    visit "/"
+
+    click_link "My Lounge"
+
+    expect(page).not_to have_content(@e1.duration_in_min)
+    expect(page).not_to have_content(@e1.workout)
+    expect(page).not_to have_content(@e1.workout_date)
+    expect(page).to have_content("No workouts yet")
+
+  end
 end
