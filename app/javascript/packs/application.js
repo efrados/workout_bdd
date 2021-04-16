97e7f9609af0ef52e6003d33edc1a99f6c43cd10
Efrain Arreche <efrados@gmail.com>
2021-04-16 16:40:40 -0300

    added flatpickr to display bbetter dates ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-// import 'bootstrap/js/src/alert'  
-// import 'bootstrap/js/src/button'  
+import 'bootstrap/js/src/alert'  
+ import 'bootstrap/js/src/button'  
 // import 'bootstrap/js/src/carousel'  
 import 'bootstrap/js/src/collapse'  
 import 'bootstrap/js/src/dropdown'  
@@ -17,8 +17,18 @@ import 'bootstrap/js/src/dropdown'
 import 'bootstrap/js/src/scrollspy'  
 // import 'bootstrap/js/src/tab'  
 // import 'bootstrap/js/src/toast'  
-// import 'bootstrap/js/src/tooltip'  
+// import 'bootstrap/js/src/tooltip'
+require("flatpickr")
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+document.addEventListener("turbolinks:load", () => {
+  flatpickr("[class='flatpickr form-control']",{
+      altInput: true,
+      altFormat:"F j Y",
+      dateFormat:"Y-m-d",
+  });
+});
+
