7473943fa3885c1d10b426f9e4d97baa140323a3
Efra <efrados@gmail.com>
2021-04-18 19:39:57 -0300

    small syntax changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -73,41 +73,41 @@ document.addEventListener("turbolinks:load", () => {
   }
 });
 
- var drawChart = function() {
-  var margin = { top: 100, right: 20, bottom: 100, left: 50 },
-      width  = 600 - margin.left - margin.right,
-      height = 450 - margin.top - margin.bottom;
-  var JSONData = document.querySelector('#chart').dataset.workouts;
- 
-  if (!JSONData) {
-    return;
-  }
-  // var data = JSONData.slice()
-  var data = JSON.parse(JSONData);
-  
-  var parseTime = d3.timeParse("%Y-%m-%d");
-  
-  var workoutFn = function(d) { return d.duration_in_min }
+  var drawChart = function() {
+    var margin = { top: 100, right: 20, bottom: 100, left: 50 },
+        width  = 600 - margin.left - margin.right,
+        height = 450 - margin.top - margin.bottom;
+    var JSONData = document.querySelector('#chart').dataset.workouts;
+   
+    if (!JSONData) {
+      return;
+    }
+    // var data = JSONData.slice()
+    var data = JSON.parse(JSONData);
+    
+    var parseTime = d3.timeParse("%Y-%m-%d");
+    
+    var workoutFn = function(d) { return d.duration_in_min }
 
-  var dateFn = function(d) { return parseTime(d.workout_date) }
-  
-  var x = d3.scaleTime()
-    .range([0, width])
-    .domain(d3.extent(data, dateFn))
-  
-  var y = d3.scaleLinear()
-    .range([height, 0])
-    .domain([0, d3.max(data, workoutFn)])
-  
-  var workout_line = d3.line()
-      .x(function(d) { return x(d.workout_date); })
-      .y(function(d) { return y(d.duration_in_min);  });
+    var dateFn = function(d) { return parseTime(d.workout_date) }
+    
+    var x = d3.scaleTime()
+      .range([0, width])
+      .domain(d3.extent(data, dateFn))
+    
+    var y = d3.scaleLinear()
+      .range([height, 0])
+      .domain([0, d3.max(data, workoutFn)])
+    
+    var workout_line = d3.line()
+        .x(function(d) { return x(d.workout_date); })
+        .y(function(d) { return y(d.duration_in_min);  });
 
-  for (let d in data) {
-    data[d].workout_date = parseTime(data[d].workout_date);
-    data[d].duration_in_min = +data[d].duration_in_min;
-  }
-// create a tooltip
+    for (let d in data) {
+      data[d].workout_date = parseTime(data[d].workout_date);
+      data[d].duration_in_min = +data[d].duration_in_min;
+    }
+    // create a tooltip
     var Tooltip = d3.select("#chart")
       .append("div")
       .style("opacity", 0)
@@ -117,30 +117,30 @@ document.addEventListener("turbolinks:load", () => {
       .style("border-width", "2px")
       .style("border-radius", "5px")
       .style("padding", "5px")
+
+    // Three function that change the tooltip when user hover / move / leave a cell
+    var mouseover = function(d) {
+      Tooltip
+        .style("opacity", 1)
+    }
+    var mousemove = function(d, workout) {
+      Tooltip
+        .html("Workout Date: " + workout.workout_date.getDate() +
+              " <br /> Duration(min): " + workout.duration_in_min +
+              " <br /> Workout Details: " + workout.workout)
+        .style("left", (d.pageX) + 15 + "px")
+        .style("top", (d.pageY) + 15 + "px")
+    }
+    var mouseleave = function(d) {
+      Tooltip
+        .style("opacity", 0)
+    }
+    var svg = d3.select("#chart").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     
-      // Three function that change the tooltip when user hover / move / leave a cell
-      var mouseover = function(d) {
-        Tooltip
-          .style("opacity", 1)
-      }
-      var mousemove = function(d, workout) {
-        Tooltip
-          .html("Workout Date: " + workout.workout_date.getDate() +
-                " <br /> Duration(min): " + workout.duration_in_min +
-                " <br /> Workout Details: " + workout.workout)
-          .style("left", (d.pageX) + 15 + "px")
-          .style("top", (d.pageY) + 15 + "px")
-      }
-      var mouseleave = function(d) {
-        Tooltip
-          .style("opacity", 0)
-      }
-  var svg = d3.select("#chart").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  
     svg.append("path")
         .attr("class", "line")
         .attr("fill", "none")
@@ -165,44 +165,44 @@ document.addEventListener("turbolinks:load", () => {
         .on("mousemove", mousemove)
         .on("mouseleave", mouseleave)
 
-  svg.append("g")
-   .attr("class", "x axis")
-   .attr("transform", "translate(0," + height + ")")
-   .call(d3.axisBottom(x)
-      .tickFormat(d3.timeFormat('%Y-%m-%d'))
-    )
-   .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", ".15em")
-      .attr("transform", "rotate(-60)");
-  
-   // x axis label
-   svg.append("text")
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x)
+        .tickFormat(d3.timeFormat('%Y-%m-%d'))
+        )
+      .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-60)");
+    
+     // x axis label
+    svg.append("text")
       .attr("x", width / 2)
       .attr("y", height + margin.top - 15)
       .style("text-anchor", "middle")
       .text("Date of workout")
-  
-   svg.append("g")
-    .attr("class", "y axis")
-    .call(d3.axisLeft(y).ticks(4));
-  
-  // y axis label
-  svg.append("text")
-     .attr("transform", "rotate(-90)")
-     .attr("y", 0 - margin.left)
-     .attr("x", 0 - (height / 2))
-     .attr("dy", "1em")
-     .style("text-anchor", "middle")
-     .text("Workout duration (min)")
-  
-   // Chat title
-   svg.append("text")
-      .attr("x", (width / 2))
-      .attr("y", 0 - (margin.top / 2))
-      .style("text-anchor", "middle")
-      .style("font-size", "18px")
-      .style("text-decoration", "underline")
-      .text("Workout duration vs Workout date")
-};
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(d3.axisLeft(y).ticks(4));
+    
+    // y axis label
+    svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+       .attr("x", 0 - (height / 2))
+       .attr("dy", "1em")
+       .style("text-anchor", "middle")
+       .text("Workout duration (min)")
+    
+     // Chat title
+     svg.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 0 - (margin.top / 2))
+        .style("text-anchor", "middle")
+        .style("font-size", "18px")
+        .style("text-decoration", "underline")
+        .text("Workout duration vs Workout date")
+  };
