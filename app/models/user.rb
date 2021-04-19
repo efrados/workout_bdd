8151b4dbc9067bae94cb881fb5d942b164c2aea8
Efra <efrados@gmail.com>
2021-04-19 01:27:15 -0300

    [hotfix] Added one space in name or last_name for search

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
    "#{first_name} #{last_name}" 
   end
   scope :with_one_name, ->(one_name){ where('LOWER(first_name) LIKE :name OR LOWER(last_name) LIKE :name', name: "%#{one_name.downcase}%") }
-  scope :with_two_names, ->(first, second){ where('(LOWER(first_name) LIKE :other_name AND LOWER(last_name) LIKE :name ) OR (LOWER(first_name) LIKE :name AND LOWER(last_name) LIKE :other_name)' , name: "%#{first.downcase}%", other_name: "%#{second.downcase}%") }
-
+  scope :with_two_names, ->(first, second){ where('((LOWER(first_name) LIKE :full_name ) OR (LOWER(last_name) LIKE :full_name)) OR (LOWER(first_name) LIKE :other_name AND LOWER(last_name) LIKE :name ) OR (LOWER(first_name) LIKE :name AND LOWER(last_name) LIKE :other_name)',
+ name: "%#{first.downcase}%", other_name: "%#{second.downcase}%", full_name: "%#{first.downcase} #{second.downcase}%") }
   def self.search(names)
     if !names.present?
       users = User.all
